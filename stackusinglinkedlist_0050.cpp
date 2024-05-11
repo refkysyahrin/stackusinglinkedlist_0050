9a79c06c49baaf362e3682ae1e06c040cce901a9
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-11 09:51:54 +0700

    4. Update the top pointer to the new node

--- stackusinglinkedlist_0050.cpp
+++ stackusinglinkedlist_0050.cpp
@@ -27,5 +27,6 @@ public:
 		Node* newNode = new Node(); // 1. Allocate memory for the new node
 		NewNode->data = value; // 2. Assign value
 		NewNode->next = data; // 3. Set the next pointer of the new node to the current top node
+		top = newNode; // 4. Update the top pointer to the new node
 	}
 };
