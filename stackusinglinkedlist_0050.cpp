82f099bfe3ec499c34b6f048101818004d129f59
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-11 09:41:52 +0700

    stack classs

--- stackusinglinkedlist_0050.cpp
+++ stackusinglinkedlist_0050.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-//Node class representing a single node in the linked list
+// Node class representing a single node in the linked list
 class Node {
 public:
 	int data;
@@ -10,4 +10,10 @@ public:
 	Node() {
 		next = NULL;
 	}
+};
+
+// Stack class
+class Stack {
+private:
+	Node* top;
 };
