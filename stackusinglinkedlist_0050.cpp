c903671baa2418a3fa93b89405eb3972620c1525
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-11 10:10:49 +0700

    Return true if the top pointer is NULL, indicating an empty stack

--- stackusinglinkedlist_0050.cpp
+++ stackusinglinkedlist_0050.cpp
@@ -59,7 +59,7 @@ public:
 
 	// IsEmpty operation: Check if the stack is empty
 	bool isEmpty() {
-
+		return top == NULL; // Return true if the top pointer is NULL, indicating an empty stack
 	}
 
 };
